9023c611428c3cb89240ee17becaa66e36fd8b4b
Swag369 <86703988+Swag369@users.noreply.github.com>
2024-03-11 10:38:04 -0400

    just pushing to remote

---
+++ frontend/src/components/Comments.js
@@ -0,0 +1,7 @@
+export default function Comments() {
+    return (
+        <div className="text-3xl font-bold underline">
+            <input placeholder="Share your thoughts"/>
+        </div>
+    )
+}
---
+++ frontend/src/components/Page.js
@@ -0,0 +1,10 @@
+import Comments from "./Comments"
+
+export default function Page() {
+    return (
+        <div className="text-3xl font-bold underline">
+            Hi
+            <Comments />      
+        </div>
+    )
+}
